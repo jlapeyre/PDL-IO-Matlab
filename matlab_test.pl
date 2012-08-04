4a0ae758311e05c570e3306d69e152bd3e624138
John Lapeyre <jlapeyre@cpan.org>
2012-08-05 01:37:21 +0200

    Implemented a bunch of things.

--- matlab_test.pl
+++ matlab_test.pl
@@ -3,17 +3,40 @@
 use warnings;
 use strict;
 use PDL;
-use PDL::IO::Matlab qw( matlab_open );
+use PDL::IO::Matlab;
 
-my $f = 'Rep_Carlo_90nm.mat';
-#my $f = 'junk';
 
-#my $res = PDL::IO::Matlab::open('afile');
-my $res = matlab_open($f);
+my $f1 = 'Rep_Carlo_90nm.mat';
+my $mat1 = PDL::IO::Matlab->new($f1, '<');
+print "Matlab file format of $f1 is " . $mat1->get_version . "\n";
+$mat1->print_all_var_info;
+$mat1->close;
 
-print $res,"\n";
+my $f2 = 'testf_mat73.mat';
+my $mat2 = PDL::IO::Matlab->new($f2, '>', { header => 'This is the header' });
+$mat2->close;
 
-PDL::IO::Matlab::close($res);
+$mat2 = PDL::IO::Matlab->new($f2, '<');
+print "Matlab file format of $f2 is " . $mat2->get_version . "\n";
+$mat2->close;
 
+my $f3 = 'testf_mat5.mat';
+my $mat3 = PDL::IO::Matlab->new($f3, '>', {format => 'MAT5'});
+$mat3->close;
+
+$mat3 = PDL::IO::Matlab->new($f3, '<');
+print "Matlab file format of $f3 is " . $mat3->get_version . "\n";
+$mat3->close;
+
+my $f4 = 'test_mat73a.mat';
+my $mat4 = PDL::IO::Matlab->new($f4, '>', {format => 'MAT73'});
+$mat4->close;
+
+$mat4 = PDL::IO::Matlab->new($f4, 'r');
+print "Matlab file version of $f4 is " . $mat4->get_version . "\n";
+print "Matlab file format of $f4 is " . $mat4->get_format . "\n";
+$mat4->close;
+
+print join('.',PDL::IO::Matlab::get_library_version()), "\n";
 
 1;
